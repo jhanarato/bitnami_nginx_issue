3fb94a3f72a925941dc7f65a535afe74c624c936
jr <jhanarato@gmail.com>
2025-11-05 16:57:29 +0800

    Add handler for SIGINT

--- backend/sleep.py
+++ backend/sleep.py
@@ -1,5 +1,13 @@
-from time import sleep
+import time
+import signal
+import sys
+
+def signal_handler(sig, frame):
+    print("Got SIGINT. Exiting.")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 while True:
-    print("In backend. Sleeping for 10 seconds")
-    sleep(10)
+    print("Backend is running.")
+    time.sleep(10)
